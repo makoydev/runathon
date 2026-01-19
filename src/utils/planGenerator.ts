951f9a0dcf9c4ed98d35c41f0e3908ba6b4946b4
Mike Mendoza <engr.chaelmendoza@gmail.com>
2026-01-19 21:10:37 +0800

    race day

--- src/utils/planGenerator.ts
+++ src/utils/planGenerator.ts
@@ -173,7 +173,7 @@ function generateWeeklyPlan(
     },
   ];
 
-  // Adjust final week for race days
+  // Adjust final week for race day
   if (weekNum === totalWeeks) {
     days[5] = {
       day: 'Saturday',
