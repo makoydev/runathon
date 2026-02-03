c8cbee2fe660b0e6ec16a033be9a08171e7eb69b
Mike Mendoza <engr.chaelmendoza@gmail.com>
2026-02-03 23:29:24 +0800

    return weeknum

--- src/utils/planGenerator.ts
+++ src/utils/planGenerator.ts
@@ -202,7 +202,7 @@ function generateWeeklyPlan(
     if (weekNum === totalWeeks && dayName === 'Sunday') continue; // never remove the race day
     downgradeToRest(dayName);
   }
-
+// return weeknum
   return {
     week: weekNum,
     phase,
