1fc2c7987dda207cdea5230937f6da53eb74911b
Mike Mendoza <engr.chaelmendoza@gmail.com>
2026-02-19 23:31:22 +0800

    add new for loop

--- src/utils/planGenerator.ts
+++ src/utils/planGenerator.ts
@@ -221,7 +221,6 @@ export function generateTrainingPlan(
   const normalizedTargetPace = secondsToPace(paceToSeconds(targetPace));
   const weeks: TrainingWeek[] = [];
 
-  //add week loop
   for (let i = 1; i <= info.weeks; i++) {
     weeks.push(generateWeeklyPlan(i, info.weeks, distance, normalizedCurrentPace, normalizedTargetPace, trainingDays));
   }
