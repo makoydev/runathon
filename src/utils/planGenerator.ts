1e8ec20008b45aea20d61e808c9f857eaf52ce13
Mike Mendoza <engr.chaelmendoza@gmail.com>
2026-04-30 17:15:34 +0800

    (prioritize keeping

--- src/utils/planGenerator.ts
+++ src/utils/planGenerator.ts
@@ -196,7 +196,7 @@ function generateWeeklyPlan(
     };
   }
 
-  // Trim to users available training days
+  // Trim to users available training days (prioritize keeping 
   const removalPriority = weekNum === totalWeeks
     ? ['Wednesday', 'Friday', 'Tuesday', 'Saturday', 'Thursday']
     : ['Friday', 'Sunday', 'Tuesday', 'Wednesday', 'Thursday', 'Saturday'];
